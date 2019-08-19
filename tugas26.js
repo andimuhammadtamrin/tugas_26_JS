e516a3cbec517a5ccaf49d00f5d8d8f2f1ba0792
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2019-08-19 22:55:33 +0800

    Tugas 26 Java Script
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas26.js
@@ -0,0 +1,13 @@
+function periksa(){
+  var a = [2,39,76,50,9,7,41,2,24,1,16]
+  for(i=0; i<a.length; i++){
+    if(isFinite(a[i])===true){
+      console.log("Angka ",a[i], "NOT Infinity");
+    }
+    else{
+      console.log("Angka ",a[i], "Infinity");
+    }
+
+  }
+}
+periksa()
